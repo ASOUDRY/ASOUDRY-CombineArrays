bc7cd7d0322eae13db696c238a98e7b233814c22
ASOUDRY <soudryalex@gmail.com>
2023-03-01 23:39:08 +0000

    done

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,18 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        int number = arr1.length + arr2.length;
+        int[] fusion = new int[number];
+        int v = 0;
+        for (int i = 0; i < number; i++) {
+            if (i < arr1.length) {
+        fusion[i] = arr1[i];
+            }
+            else {
+                 fusion[i] = arr2[v];
+                 v = v + 1;
+            }
+        }    
+    return fusion;
     }
 }
